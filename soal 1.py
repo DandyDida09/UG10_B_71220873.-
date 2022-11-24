35ec4e3385cfcebc027233bf487910374cb484f0
DandyDida09 <117884467+DandyDida09@users.noreply.github.com>
2022-11-24 13:02:08 +0700

    Add files via upload

---
+++ soal 1.py
@@ -0,0 +1,6 @@
+print ("PROGRAM PENGHITUNG VOLUME RUANG")
+P = float (input ("masukan panjang sisi alas limas: "))
+l = float (input ("masukan tinggi limas:"))
+t = float (input ("masukan limas tersebut adalah"))
+v = P*l*t 
+print ("volume limas = %0.2f" %v)
